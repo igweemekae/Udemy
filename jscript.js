41263eeaa381d68f1255b57ff2e5ab94e2d7586c
Ezeo <igweemekae@gmail.com>
2022-05-24 12:15:33 +0100

    created newly locally

---
+++ jscript.js
@@ -0,0 +1,12 @@
+const firstName = "Eze";
+const lastName = "Igwe";
+
+console.log(`my name is ${firstName} ${lastName}. God bless you for reaching out!`)
+
+const now = 2022;
+const Ebuka = now - 2001;
+const Chisom = now - 1998;
+const Amaka = now - 2003;
+
+console.log(Ebuka, Amaka, Chisom)
+console.log(Ebuka  + Amaka + Chisom <= 100);
